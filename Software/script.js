9cd001c6ee771e538f37b857f1248f9bfcdce17d
025528078pP <peachzxzx@hotmail.com>
2019-07-12 12:29:09 +0700

    add some updates

--- Software/script.js
+++ Software/script.js
@@ -1,19 +1,44 @@
-let data_stucture = {email: user_data}
-let user_data = {fullname: "",
-    surname:"",
-    gender:"",
-    age:"",
-    pill_time: "",
-    activity: activity_data}
-let activity_data = {activity_name = {time_start: "", time_stop: "", color: ""}}
+//let data_stucture = {email: user_data}
+//let user_data = {fullname: "",
+//    surname:"",
+//    gender:"",
+//    age:"",
+//    pill_time: {},
+//    activity: activity_data}
+//let activity_data = {activity_name : {time_start: "", time_stop: "", color: ""}}
 
-function POST() {
-    let email = document.getElementById("EEMM").value
+function Submit_data() {
     const url = "https://exceed.superposition.pknn.dev/data/15"
+    fetch(url)
+    .then(function(response) {
+    return response.json();
+  })
+  .then(function(myJson) {
+    let Known_data = myJson
+    let Username = document.getElementById("username").value
+    let user_data = {"fullname": document.getElementById("fullname").value,
+        "surname":document.getElementById("username").value,
+        "gender":document.getElementById("gender").value,
+        "age":document.getElementById("age").value,
+        "pill_time": document.getElementById("pill_time").value,
+        "tele_num" : "",
+        "Emer_tele_num" : "",
+        "activity": {}
+    }
+    Known_data[Username] = user_data
+    POST(Known_data)
+    }
+  );
+
+    
+}
+
+function POST(data) {
+    const url = "https://exceed.superposition.pknn.dev/data/15"
+    console.log("Hello world")
     fetch(url, {
         method: 'POST',
-        body: JSON.stringify({"data" :{
-            "Email": email}
+        body: JSON.stringify({"data" :data
         }),
         headers:{
         'Content-Type': 'application/json'}
@@ -30,7 +55,12 @@ function GET() {
   })
   .then(function(myJson) {
     let text = JSON.stringify(myJson);
-    document.getElementById('HH').innerHTML += `<h3>${text}</h3>`
+    document.getElementById('GGWP').innerHTML += `<h3>${text}</h3>`
 }
   );
+}
+
+function reset() {
+    let data = {}
+    POST(data)  
 }
