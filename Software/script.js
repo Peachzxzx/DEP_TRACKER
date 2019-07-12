bc7637f8fb17f0d3e228722f1ee3dc120aeaf775
025528078pP <peachzxzx@hotmail.com>
2019-07-12 11:06:49 +0700

    add function

--- Software/script.js
+++ Software/script.js
@@ -1,1 +1,27 @@
-console.log("Hello World")
+function POST() {
+    let email = document.getElementById("EEMM").value
+    const url = "https://exceed.superposition.pknn.dev/data/15"
+    fetch(url + '', {
+        method: 'POST',
+        body: JSON.stringify({"data" :{
+            "Email": email}
+        }),
+        headers:{
+        'Content-Type': 'application/json'}
+    }).then((res) => res.json())
+    .then((data) => console.log(data))
+    .then((err) => console.log(err))
+}
+
+function GET() {
+    const url = "https://exceed.superposition.pknn.dev/data/15"
+    fetch(url + '')
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(myJson) {
+    let text = JSON.stringify(myJson);
+    document.getElementById('HH').innerHTML += `<h3>${text}</h3>`
+}
+  );
+}
