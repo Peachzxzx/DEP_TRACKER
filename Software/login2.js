e0bfe8ab4fd2b3f45600c87b315c3d2171cbbd56
025528078pP <peachzxzx@hotmail.com>
2019-07-13 00:03:38 +0700

    Major Change

--- Software/login2.js
+++ Software/login2.js
@@ -1,13 +1,15 @@
 let user = JSON.parse(localStorage['myUser2'])
 console.log(user)
-let doctorID=88
-function doclog(){
-    if(document.getElementById('doctor').value=='88'){
+let doctorID = 88
+
+function doclog() {
+    if (document.getElementById('doctor').value == '88') {
         window.location.href = "doctor2.html";
     }
 }
-function login(){
-    if(document.getElementById('login')==user[0].username){
+
+function login() {
+    if (document.getElementById('login').value == user[0].username) {
         window.location.href = "activity.html";
     }
 }
